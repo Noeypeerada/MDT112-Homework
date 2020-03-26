b4388237990bc3c1ff0229f0ba6585383b23b1ce
noeypeerada <Noeypeerada12@gmail.com>
2020-03-27 00:17:05 +0700

    Act9

---
+++ Assignment 3/3-9/3-9.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+  int x = 0;
+  int Id = 6 + 1 + 0 + 8 + 0 + 5 + 0 + 1 + 6 + 1 + 2;
+  while (x <= 1000) {
+    x++;
+    if (x % Id == 0) {
+      Serial.println(x);
+    }
+  }
+}
+void loop() {}
