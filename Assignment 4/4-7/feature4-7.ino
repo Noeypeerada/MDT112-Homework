a7dbeb2677e32bf3eb71d05db2742652be7e24bd
noeypeerada <Noeypeerada12@gmail.com>
2020-04-06 20:42:39 +0700

    Act7

--- Assignment 4/4-7/feature4-7.ino
+++ Assignment 4/4-7/feature4-7.ino
@@ -1,6 +1,35 @@
 String multiplication(int multiple){
-    String result;
 
+    String result ,sum,sum2;
 
-    return result;
+    int calculation;
+
+  for(int i=1 ;i <= 12 ;i++){
+
+    calculation = i*multiple;
+
+  if(i>=12){
+
+    sum = "\t" + String(multiple)+   "\t*\t"  +String(i)+
+
+    "\t=\t"  +String(calculation)+  "\n" ;
+
+  }
+
+   else{
+
+    sum = "\t"+ String(multiple)+   "\t*\t"   +String(i)+
+
+    "\t=\t"  +String(calculation)+  "\n"  ;
+
+   }
+
+   sum2 = sum2+sum;
+
+  }
+
+  result = sum2;
+
+   return result;
+
 }
